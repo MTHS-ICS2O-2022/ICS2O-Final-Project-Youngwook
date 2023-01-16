a2f67a906a26d74d8dc268d697340b5971703e5a
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2023-01-16 19:48:27 +0000

    base

---
+++ js/game.js
@@ -0,0 +1,54 @@
+/* Global Phaser */
+
+// Copyright (c) Youngwook All rights reserved
+//
+// Created by: Youngwook
+// Created on: Nov 2022
+// This file contains the JS functions for index.html
+
+// Scene import statements
+import SplashScene from './splashScene.js'
+import TitleScene from "./titleScene.js"
+import MenuScene from "./menuScene.js"
+import GameScene from "./gameScene.js"
+
+// Create the new scenes
+const splashScene = new SplashScene()
+const titleScene = new TitleScene()
+const menuScene = new MenuScene()
+const gameScene = new GameScene()
+
+/**
+ * Start Phaser Game
+ **/
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false,
+    },
+  },
+  //set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    //we place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+// comsole.log(game)
+
+// load scenes
+// NOTE: remember any "key" is global and can't be reused!
+game.scene.add("splashScene", splashScene)
+game.scene.add("titleScene", titleScene)
+game.scene.add("menuScene", menuScene)
+game.scene.add("gameScene", gameScene)
+
+// start title
+game.scene.start("splashScene")
