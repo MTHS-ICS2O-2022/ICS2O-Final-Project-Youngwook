b63e98e5ce0f200674168ea4ff09360fb5dd077b
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2023-09-10 22:43:10 +0000

    test

--- js/gameScene.js
+++ js/gameScene.js
@@ -546,7 +546,7 @@ class GameScene extends Phaser.Scene {
               console.log(this.alienGroup.children.entries.length)
 
               this.alienGroup.children.each(function(item) {
-                item.body.setVelocity(item.body.velocity.x)
+                item.body.setVelocity(item.body.velocity.x - 10)
               })
 
               this.targetGroup.children.each(function(item) {
