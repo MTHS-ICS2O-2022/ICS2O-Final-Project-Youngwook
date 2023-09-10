1a1ecf1688d3e909247d0a8a31de6abb4308194e
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2023-09-10 19:20:33 +0000

    3rd ability test

--- js/gameScene.js
+++ js/gameScene.js
@@ -547,7 +547,7 @@ class GameScene extends Phaser.Scene {
               console.log(this.alienGroup.children.entries.length)
 
               this.alienGroup.children.each(function(item) {
-                console.log(item[0])
+                console.log(item)
               })
 
               this.targetGroup.children.each(function(item) {
